2a8ef35b87b822ca5e036a4d0b3aaeec5c0dee90
jioushan <jioushan@users.noreply.github.com>
2026-05-02 13:29:07 +0000

    deploy: 2cd03f2 2cd03f28c440211853f4baee3dd58c063dc54c62

---
+++ assets/Communities_index.md.DzASsVrB.js
@@ -0,0 +1,31 @@
+import{_ as a,o as i,c as n,ag as e}from"./chunks/framework.BmLMQRXF.js";const o=JSON.parse('{"title":"AS12198｜Communities","description":"","frontmatter":{},"headers":[],"relativePath":"Communities/index.md","filePath":"Communities/index.md"}'),l={name:"Communities/index.md"};function p(t,s,h,k,r,d){return i(),n("div",null,s[0]||(s[0]=[e(`<h1 id="as12198-communities" tabindex="-1">AS12198｜Communities <a class="header-anchor" href="#as12198-communities" aria-label="Permalink to &quot;AS12198｜Communities&quot;">​</a></h1><h2 id="region-code-router-id" tabindex="-1">Region Code ROUTER_ID: <a class="header-anchor" href="#region-code-router-id" aria-label="Permalink to &quot;Region Code ROUTER_ID:&quot;">​</a></h2><div class="language-bash vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">bash</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">Asia:3x</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">NRT:33</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">NRT:40</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">OSA:34</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">---</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">SGP:32</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">---</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">HKG:31</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">HKG:35</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">HKG:36</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">6ff9.76r:37</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">CGO:371</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">MCO:38</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">---</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">TPE:39</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">----</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">EU:1x</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">AMS:11</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">FRA:12</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">----</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">US:2x</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">FMT:20</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">KAS:21</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">SJC:22</span></span></code></pre></div><h2 id="example" tabindex="-1">Example: <a class="header-anchor" href="#example" aria-label="Permalink to &quot;Example:&quot;">​</a></h2><div class="language-yaml vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">yaml</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">bgp_community.add((12198, 33));</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">if (12198, 34) ~ bgp_community then bgp_local_pref = 500;</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">bgp_path.prepend(LOCAL_ASN);</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">bgp_large_community.add((LOCAL_ASN, ROUTER_ID, LOCAL_ASN));</span></span></code></pre></div><h2 id="more" tabindex="-1">More: <a class="header-anchor" href="#more" aria-label="Permalink to &quot;More:&quot;">​</a></h2>`,6)]))}const F=a(l,[["render",p]]);export{o as __pageData,F as default};
