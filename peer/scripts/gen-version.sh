9eb345af04912ece92712eae1e2c2788042eeb76
Jioushan <jioushan182@yahoo.co.jp>
2026-05-03 00:14:51 +0900

    0

--- peer/scripts/gen-version.sh
+++ peer/scripts/gen-version.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 mkdir -p docs/public
 COMMIT=${COMMIT_REF:-$(git rev-parse --short HEAD 2>/dev/null || echo "unknown")}
 echo "{\"commit\":\"${COMMIT:0:7}\"}" > docs/public/version.json
